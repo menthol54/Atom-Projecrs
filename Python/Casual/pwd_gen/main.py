1b8589303c7d35585d988e3dc71acd1f2dcc92ef
menthol54 <menthol54@protonmail.com>
2021-04-14 19:36:24 -0500

    h

--- Python/Casual/pwd_gen/main.py
+++ Python/Casual/pwd_gen/main.py
@@ -1,16 +1,22 @@
 import random
 from indexes import characters
 from indexes import symbols
-5
+
+def length():
+    user = int(input("Length: "))
+    if user > 5:
+        pass
+    else:
+        print('Not working')
+
+
 def main():
+    length()
     letter = ""
     for i in range(1):
         i = random.choice(characters and symbols)
         letter += i
         print(letter)
-
-
-
 
 if __name__ == '__main__':
     main()
