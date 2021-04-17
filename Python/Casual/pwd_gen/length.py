b1f8af33e4b07665a99b27fc6d8fb40bdb4fbe1d
menthol54 <menthol54@protonmail.com>
2021-04-17 18:57:57 -0500

    k

--- Python/Casual/pwd_gen/length.py
+++
@@ -1,8 +0,0 @@
-def Length():
-    user = int(input("Length: "))
-    len = ""
-    if user >= 8:
-        len += len(len)
-        StopIteration
-    else:
-        print('Not working')
--- Python/Casual/pwd_gen/rand.py
+++
@@ -1,9 +0,0 @@
-from random import choice
-
-
-def Rand():
-    letter = ""
-    i = random.choice(characters and symbols)
-    for i in range():
-        letter += i
-        print(letter)
