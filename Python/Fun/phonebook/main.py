cca064f540b546d2ed93b9900286481675b63f9f
menthol54 <menthol54@protonmail.com>
2021-04-24 15:13:54 -0500

    i

--- Python/Fun/phonebook/main.py
+++ Python/Fun/phonebook/main.py
@@ -1,4 +1,4 @@
-#User inputs a code
+# User inputs a code
 user_input = input('''>> Welcome to your phonebook
 >> Type 1 to create a new contact
 >> Type 2 to edit a contact
@@ -6,12 +6,19 @@ user_input = input('''>> Welcome to your phonebook
 >> Type 4 to delete a contact
 >> What would you like to do: ''')
 contact = user_input
-#Fuction one creates a contact
+global person
+person = {}
+# Fuction one creates a contact
+
 
 def create():
     if contact == '1':
         name = input('Name: ')
         num = input('Number: ')
-        person = {}
         person.update({name: num})
         print(person)
+create()
+
+def edit():
+    if contact == '2':
+        pass
