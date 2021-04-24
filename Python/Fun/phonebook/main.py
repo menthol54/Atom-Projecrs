f7771dad72352fb3b509a4b7fd90f48880ed4008
menthol54 <menthol54@protonmail.com>
2021-04-24 14:33:50 -0500

    t

---
+++ Python/Fun/phonebook/main.py
@@ -0,0 +1,18 @@
+#User inputs a code
+user_input = input('''>> Welcome to your phonebook
+>> Type 1 to create a new contact
+>> Type 2 to edit a contact
+>> Type 3 to view a contact
+>> Type 4 to delete a contact
+>> What would you like to do: ''')
+contact = user_input
+#Fuction one creates a contact
+
+def one():
+    if contact == '1':
+        name = input('Name: ')
+        num = input('Number: ')
+        person = {}
+        person.update({name: num})
+        print(person)
+one()
