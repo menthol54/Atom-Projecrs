332cbeeba1b534e56821ceeb9bb535c4b3d4660f
menthol54 <menthol54@protonmail.com>
2021-04-25 18:05:03 -0500

    k

--- Python/Fun/phonebook/main.py
+++ Python/Fun/phonebook/main.py
@@ -1,27 +1,39 @@
 # User inputs a code
-user_input = input('''>> Welcome to your phonebook
->> Type 1 to create a new contact
->> Type 2 to edit a contact
->> Type 3 to view a contact
->> Type 4 to delete a contact
->> What would you like to do: ''')
-contact = user_input
+def enter():
+    user_input = int(input('''>> Welcome to your phonebook
+    >> Type 1 to create a new contact
+    >> Type 2 to edit a contact
+    >> Type 3 to view a contact
+    >> Type 4 to delete a contact
+    >> What would you like to do: '''))
+    return user_input
+
+
+
 global person
 person = []
 # Fuction one creates a contact
-def create():
-    if contact == '1':
-        name = input('Name: ')
-        num = int(input('Number: '))
 
-        person.append(name + ':' + num)
+
+def create():
+    while True:
+        contact = enter()
+    if contact == 1:
+        name = str(input('Name: '))
+        num = int(input('Number: '))
+        person.append(name + ':' + str(num))
         print(person)
+
+
 create()
-#create a function to edit
+# create a function to edit
+
+
 def edit():
     if contact == '2':
         pass
+
+
 def view():
     if contact == '3':
-        print(person)
-view()
+        pass
