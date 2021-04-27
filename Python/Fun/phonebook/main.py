3d19cb18702737add819f3ca0311f4c8eb8cb233
menthol54 <menthol54@protonmail.com>
2021-04-26 20:33:39 -0500

    j

--- Python/Fun/phonebook/main.py
+++ Python/Fun/phonebook/main.py
@@ -1,39 +1,35 @@
-# User inputs a code
-def enter():
-    user_input = int(input('''>> Welcome to your phonebook
-    >> Type 1 to create a new contact
-    >> Type 2 to edit a contact
-    >> Type 3 to view a contact
-    >> Type 4 to delete a contact
-    >> What would you like to do: '''))
-    return user_input
-
-
-
+#User inputs a code
+user_input = input('''>> Welcome to your phonebook
+>> Type 1 to create a new contact
+>> Type 2 to view a contact
+>> Type 3 to edit a contact
+>> Type 4 to delete a contact
+>> What would you like to do: ''')
+contact = user_input
+#Fuction one creates a contact
 global person
-person = []
-# Fuction one creates a contact
-
-
+person = {}
 def create():
-    while True:
-        contact = enter()
-    if contact == 1:
-        name = str(input('Name: '))
-        num = int(input('Number: '))
-        person.append(name + ':' + str(num))
+    if contact == '1':
+        name = input('Name: ')
+        num = input('Number: ')
+        person.update({name : num})
         print(person)
 
+        f = open(f'contacts\ {name}.txt', 'a')
+        f.write(str(person))
+        f.close()
 
+
+        file = open('contacts.txt', 'a')
+        file.write(str(person))
+        file.close()
 create()
-# create a function to edit
-
-
-def edit():
-    if contact == '2':
-        pass
-
 
 def view():
-    if contact == '3':
-        pass
+    if contact == '2':
+        name = input("Name: ")
+        f = open(f'contacts {name}', 'r')
+        print(f.read())
+    else: print('I was unable to find it.')
+view()
